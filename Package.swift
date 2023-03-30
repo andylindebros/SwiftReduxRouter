7ac2672a9a7234191a87871bdd1e6e64a2475ecf
Andreas Linde <andylindebros@gmail.com>
2023-03-31 01:02:44 +0200

    Removed dependencies

--- Package.swift
+++ Package.swift
@@ -16,17 +16,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/lindebrothers/Logger", from: "1.1.0"),
+        //.package(url: "https://github.com/lindebrothers/Logger", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SwiftReduxRouter",
-            dependencies: [
-                "Logger",
-            ]
-        ),
+        .target(name: "SwiftReduxRouter"),
         .testTarget(
             name: "SwiftReduxRouterTests",
             dependencies: ["SwiftReduxRouter"]
