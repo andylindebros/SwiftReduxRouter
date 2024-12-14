a653d3f1f238bd256aea5879165cd97db3cdcde7
Andreas Linde <andylindebros@gmail.com>
2024-12-14 10:11:11 +0100

    Added more functionality

--- Sources/SwiftReduxRouter/Utils/URLExtensions.swift
+++ Sources/SwiftReduxRouter/Utils/URLExtensions.swift
@@ -10,13 +10,12 @@ public extension URL {
     }
 
     func addJSONQueryItem<Model: Encodable>(key: String, value: Model) -> URL {
-        let encoder = JSONEncoder()
         if
             var components = URLComponents(url: self, resolvingAgainstBaseURL: true),
-            let modelData = try? encoder.encode(value),
-            let value = String(data: modelData, encoding: .utf8)
+
+            let item = URLQueryItem.createJSONQueryItem(key: key, value: value)
         {
-            components.queryItems?.append(URLQueryItem(name: key, value: value))
+            components.queryItems?.append(item)
             return components.url ?? self
         }
         return self
@@ -31,6 +30,16 @@ public extension URL {
         }
         return self
     }
+
+    func setQueryItems(to newValue: [URLQueryItem]) -> URL {
+        if
+            var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        {
+            components.queryItems = newValue
+            return components.url ?? self
+        }
+        return self
+    }
 }
 
 public extension URLQueryItem {
@@ -40,6 +49,17 @@ public extension URLQueryItem {
             let value = value?.data(using: .utf8)
         {
             return try? decoder.decode(Model.self, from: value)
+        }
+        return nil
+    }
+
+    static func createJSONQueryItem<Model: Encodable>(key: String, value: Model) -> URLQueryItem? {
+        let encoder = JSONEncoder()
+        if
+            let modelData = try? encoder.encode(value),
+            let value = String(data: modelData, encoding: .utf8)
+        {
+           return URLQueryItem(name: key, value: value)
         }
         return nil
     }
